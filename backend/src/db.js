0c30256e64877b7e42f4bcbec5f60e4b3c1f9241
Rubén Dario Barragan España <rbarragan1992@utm.edu.ec>
2026-01-07 20:59:33 -0500

    Update db.js
    fix db connection for render

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,12 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 const { Pool } = pkg;
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "inventario_db",
-  password: process.env.DB_PASSWORD || "",
-  port: Number(process.env.DB_PORT || 5432),
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProd ? { rejectUnauthorized: false } : false,
 });
